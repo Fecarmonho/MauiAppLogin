d6c572893e949439c5ae93cc60f78114d0a43112
Fecarmonho <fe.carmonho@gmail.com>
2024-10-24 10:47:32 -0300

    Add arquivos ao projeto

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -8,5 +8,13 @@
 
             MainPage = new AppShell();
         }
-    }
+
+        protected override Window CreateWindow(IActivationState activityState)
+        {
+            var window = base.CreateWindow(activityState);
+            window.Width = 400;
+            window.Height = 600;
+            return window;
+        }
+    } // Fecha Classe
 }
